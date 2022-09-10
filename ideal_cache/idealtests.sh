92cb9b3a4188fa8d106082513473d25c7ca9a375
Maxim <maxmosk@list.ru>
2022-09-10 17:46:04 +0300

    Started ideal cache

---
+++ ideal_cache/idealtests.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+I=1
+while IFS=% read -r source exp; do
+        res=$(echo $source | $1)
+        if [ x"$exp" != x"$res" ]; then
+                echo TEST $I '('$source')' FAILED: expexted "$exp", got "$res"
+
+        else
+                echo TEST $I PASSED
+        fi
+        I=$((I+1))
+done <<END
+END
+
