b5c610aa998f007f7d98628f87d7a7ac7307fea5
Maxim <maxmosk@list.ru>
2022-09-10 18:41:23 +0300

    Add constr and destr for ideal cache

--- ideal_cache/src/head/ideal.hpp
+++ ideal_cache/src/head/ideal.hpp
@@ -4,7 +4,21 @@
 template <typename elem_t>
 class ideal_cache
 {
+    size_t size;
+    size_t used;
+    elem_t *cells;
+
     public:
+        ideal_cache(size_t initSize) : size (initSize), used (0)
+        {
+            cells = new elem_t[size];
+        }
+
+
+        ~ideal_cache()
+        {
+            delete[] cells;
+        }
 };
 
 #endif
