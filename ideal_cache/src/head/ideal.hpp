9067278ef8e546fbd480feff12ac2fa89cc4896d
Maxim <maxmosk@list.ru>
2022-09-10 19:00:18 +0300

    Add page sequence to ideal cache

--- ideal_cache/src/head/ideal.hpp
+++ ideal_cache/src/head/ideal.hpp
@@ -1,17 +1,29 @@
 #ifndef __IDEAL_HPP_INCLUDED__
 #define __IDEAL_HPP_INCLUDED__
 
+#include <vector>
+
+
 template <typename elem_t>
 class ideal_cache
 {
     size_t size;
     size_t used;
     elem_t *cells;
+    
+    std::vector<elem_t> pageSequence;
 
     public:
         ideal_cache(size_t initSize) : size (initSize), used (0)
         {
             cells = new elem_t[size];
+            pageSequence.reserve(size * 10);
+        }
+
+
+        size_t getSize() const
+        {
+            return size;
         }
 
 
