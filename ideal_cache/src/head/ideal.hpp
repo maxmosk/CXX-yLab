2ef439312692ddea8d39b5d630d718aa73ea121a
Maxim <maxmosk@list.ru>
2022-10-29 23:06:00 +0300

    Add namespace to ideal

--- ideal_cache/src/head/ideal.hpp
+++ ideal_cache/src/head/ideal.hpp
@@ -7,128 +7,125 @@
 #include <cstdint>
 
 
-template <typename elem_t>
-class ideal_cache final
+namespace cache
 {
-    //================================== Private
-    size_t size;
-    size_t used = 0;
-    elem_t *cells;
-    
-    std::vector<elem_t> sequence;
-    size_t curPos = 0;
-
-    std::unordered_map<elem_t, size_t> finderCache;
-
-
-    size_t lookupToReplace()
+    template <typename elem_t>
+    class ideal_cache final
     {
-        //printf("*** %zu < %zu?\n", used, size);
-        if (used < size)
-        {
-            return used;
-        }
+        //================================== Private
+        size_t size;
+        size_t used = 0;
+        std::vector<elem_t> cells;
+        
+        std::vector<elem_t> sequence;
+        size_t curPos = 0;
 
-        else
-        {
-            size_t foundIndex = SIZE_MAX;
-            size_t foundEntry = 0;
-            size_t seqLen = sequence.size();
+        std::unordered_map<elem_t, size_t> finderCache;
 
-            for (size_t i = 0; i < used; i++)
+
+        size_t lookupToReplace()
+        {
+            if (used < size)
             {
-                size_t curEntry = SIZE_MAX;
-                for (size_t j = curPos; j < seqLen; j++)
+                return used;
+            }
+
+            else
+            {
+                size_t foundIndex = SIZE_MAX;
+                size_t foundEntry = 0;
+                size_t seqLen = sequence.size();
+
+                for (size_t i = 0; i < used; i++)
                 {
-                    if (sequence[j] == cells[i])
+                    size_t curEntry = SIZE_MAX;
+                    for (size_t j = curPos; j < seqLen; j++)
                     {
-                        curEntry = j;
-                        break;
+                        if (sequence[j] == cells[i])
+                        {
+                            curEntry = j;
+                            break;
+                        }
+                    }
+
+                    if (foundEntry < curEntry)
+                    {
+                        foundEntry = curEntry;
+                        foundIndex = i;
                     }
                 }
 
-                if (foundEntry < curEntry)
+                return foundIndex;
+            }
+        }
+        
+        bool lookup(elem_t elem)
+        {
+            auto search = finderCache.find(elem);
+            if (search != finderCache.end())
+            {
+                return true;
+            }
+
+            else
+            {
+                insert(elem);
+                return false;
+            }
+        }
+
+        void insert(elem_t elem)
+        {
+            if (size == 0)
+            {
+                return;
+            }
+
+            else
+            {
+                size_t indexFind = lookupToReplace();
+                
+                finderCache.erase(cells[indexFind]);
+                cells[indexFind] = elem;
+                finderCache[elem] = indexFind;
+
+                if (indexFind < size)
                 {
-                    foundEntry = curEntry;
-                    foundIndex = i;
+                    used++;
                 }
             }
-
-            //printf("*** Founded index %zu", foundIndex);
-            return foundIndex;
-        }
-    }
-    
-    bool lookup(elem_t elem)
-    {
-        auto search = finderCache.find(elem);
-        if (search != finderCache.end())
-        {
-            return true;
         }
 
-        else
-        {
-            insert(elem);
-            return false;
-        }
-    }
 
-    void insert(elem_t elem)
-    {
-        if (size == 0)
+    public:
+        //================================== Public
+        ideal_cache(size_t initSize) : size (initSize), used (0)
         {
-            return;
+            cells = std::vector(size);
+            sequence = std::vector(size * 10);
         }
 
-        else
-        {
-            size_t indexFind = lookupToReplace();
-            //printf("*** Replaced %d for %d by index %zu\n", cells[indexFind], elem, indexFind);
-            finderCache.erase(cells[indexFind]);
-            cells[indexFind] = elem;
-            finderCache[elem] = indexFind;
 
-            if (indexFind < size)
+        size_t getHits()
+        {
+            size_t hits = 0;
+            size_t nelems = sequence.size();
+            for (; curPos < nelems; curPos++)
             {
-                used++;
+                hits += lookup(sequence[curPos]);
             }
+
+            return hits;
         }
-    }
 
-public:
-    //================================== Public
-    ideal_cache(size_t initSize) : size (initSize), used (0)
-    {
-        cells = new elem_t[size];
-        memset(cells, 0, size * sizeof (elem_t));
-        sequence.reserve(size * 10);
-    }
-
-
-    size_t getHits()
-    {
-        size_t hits = 0;
-        size_t nelems = sequence.size();
-        for (; curPos < nelems; curPos++)
+        void putElem(const elem_t &src)
         {
-            hits += lookup(sequence[curPos]);
+            sequence.push_back(src);
         }
 
-        return hits;
-    }
 
-    void putElem(const elem_t &src)
-    {
-        sequence.push_back(src);
-    }
-
-
-    ~ideal_cache()
-    {
-        delete[] cells;
-    }
-};
+        ~ideal_cache() = default;
+    };
+}
 
 #endif // IDEAL_HPP_INCLUDED__
-
